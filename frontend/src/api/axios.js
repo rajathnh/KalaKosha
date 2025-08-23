565690cb25f86e4c60a81b2c5e0123ebb5bf9ca7
rajathnh <rajathnh2@gmail.com>
2025-08-23 10:33:54 +0530

    Production Frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 // src/api/axios.js
 import axios from 'axios';
-
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
 // Create a new instance of axios with a custom configuration
 const apiClient = axios.create({
   // This is the base URL of your backend server
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: baseURL,
   
   // This is CRITICAL for sending the httpOnly cookie with requests
   withCredentials: true, 
