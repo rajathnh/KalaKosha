f4548f664dfd58b6aabda5f24c439ab253e68a51
rajathnh <rajathnh2@gmail.com>
2025-08-23 11:00:20 +0530

    Production Backend

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -21,6 +21,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
+    sameSite: 'none',
   });
 };
 
